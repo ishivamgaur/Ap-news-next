703c86cb835f84486cd3997d58add56675ec4ed3
iamshivamgaur <shivam24august@gmail.com>
2025-11-28 10:19:17 +0530

    Changed: BASE_URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5000/api", // Your backend server URL
+  baseURL: "https://ap-news-b.onrender.com/api", // Your backend server URL
 });
 
 export default apiClient;
